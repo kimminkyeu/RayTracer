4360907d9171ad9f1e5e49bbe9bfeb1447adceed
Photogrammer <khsj15@khu.ac.kr>
2022-09-07 19:55:32 +0900

    feat: :sparkles: 그라데이션이 되는 페인터 구현
    페인터에서 white 색상만 아닌 rgb  그라데이션이 가능한 코드로 변경하였음.

--- myapp/examples/ex02_painter/render_viewport.c
+++ myapp/examples/ex02_painter/render_viewport.c
@@ -6,7 +6,7 @@
 /*   By: sungjpar <sungjpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 23:30:53 by minkyeki          #+#    #+#             */
-/*   Updated: 2022/09/07 17:17:13 by sungjpar         ###   ########seoul.kr  */
+/*   Updated: 2022/09/07 19:54:40 by sungjpar         ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,47 @@ int	render_viewport(t_device *device, t_image *viewport)
 	/** NOTE: Use Static Variable for private data member! */
 	static	t_vec2 pressed_location;
 	static	t_vec2 second_location;
-	static	int	lock = 0;
-
-	if (input_is_mouse_down(device, MOUSE_RIGHT_CLICK))
+	static	int	lock = -1;
+	static	int color;
+	static	int	r,g,b;
+	if (input_is_mouse_down(device, MOUSE_LEFT_CLICK))
 	{
-		if (lock == 0)
+		if (lock == 0 || lock == -1)
 		{
 			pressed_location = input_get_mouse_pos(device);
 			lock = 1;
 		}
+		if (lock == -1)
+		{
+			r = 255;
+			g = 0;
+			b = 0;
+		}
 		second_location = input_get_mouse_pos(device);
-		gl_draw_line(viewport, pressed_location, second_location, WHITE);
+		if (r > 0 && b <= 0)
+		{
+			color = gl_color(BLACK, r--, g++, 0);
+		}
+		else if (g > 0 && r <= 0)
+		{
+			color = gl_color(BLACK, 0, g--, b++);
+		}
+		else if (b > 0 && g <= 0)
+		{
+			g = 0;
+			color = gl_color(BLACK, r++, 0, b--);
+		}
+		else
+		{
+			color = gl_color(BLACK, 255, 0, 0);
+			r = 255;
+			g = 0;
+			b = 0;
+		}
+		gl_draw_line(viewport, pressed_location, second_location, color);
 		pressed_location = second_location;
 	}
-	else if (input_is_mouse_unpressed(device, MOUSE_RIGHT_CLICK))
+	else if (input_is_mouse_unpressed(device, MOUSE_LEFT_CLICK))
 	{
 		/** test = GREEN; */
 		if (pressed_location.x == second_location.x && pressed_location.y == second_location.y)
@@ -44,4 +71,3 @@ int	render_viewport(t_device *device, t_image *viewport)
 	}
 	return (0);
 }
-
