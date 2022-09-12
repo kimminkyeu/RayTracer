fffae2887093dd7e3917d33fdfcd8ae4f99819c5
Kyeu <minkyeki@student.42seoul.kr>
2022-09-12 21:09:03 +0900

    feat: added gl_image_filter.h and it's source

---
+++ engine/gl_lib/image_filter/gl_image_filter.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gl_image_filter.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minkyeki <minkyeki@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 21:03:32 by minkyeki          #+#    #+#             */
+/*   Updated: 2022/09/12 21:06:52 by minkyeki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef IMAGE_FILTER_H
+# define IMAGE_FILTER_H
+
+# include "gl_device.h"
+# include "gl_color.h"
+
+
+#endif /* IMAGE_FILTER_H */
