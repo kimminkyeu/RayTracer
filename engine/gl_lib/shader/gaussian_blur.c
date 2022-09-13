ec52fccdbc36e8029e51f03bf3883b680aecf165
Kyeu <minkyeki@student.42seoul.kr>
2022-09-13 18:16:40 +0900

    가우시안 블러 구현 시작

--- engine/gl_lib/shader/gaussian_blur.c
+++ engine/gl_lib/shader/gaussian_blur.c
@@ -1,15 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   gausian_blur.c                                     :+:      :+:    :+:   */
+/*   gaussian_blur.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minkyeki <minkyeki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 21:04:28 by minkyeki          #+#    #+#             */
-/*   Updated: 2022/09/13 13:07:34 by minkyeki         ###   ########.fr       */
+/*   Updated: 2022/09/13 18:14:50 by minkyeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "gl_shader.h"
 
+void	gl_image_gaussian_blur(t_image *img)
+{
+	int	i;
+	int j;
 
+	t_vec4	neighbor_color_sum;
+
+	neighbor_color_sum = gl_vec4(0.0f, 0.0f, 0.0f, 1.0f);
+
+	while (j < img->img_size.height)
+	{
+		while (i < img->img_size.width)
+		{
+			t_vec4 neighbor_color
+
+			i++;
+		}
+	}
+}
