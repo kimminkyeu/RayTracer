ac02ff4006a554c95f4d5dd5ae4efbc608f67d09
Kyeu <minkyeki@student.42seoul.kr>
2022-10-10 17:41:26 +0900

    done

--- raytracer/parse_rt_file_to_device.c
+++ raytracer/parse_rt_file_to_device.c
@@ -6,7 +6,7 @@
 /*   By: minkyeki <minkyeki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:35:05 by minkyeki          #+#    #+#             */
-/*   Updated: 2022/10/10 17:19:05 by minkyeki         ###   ########.fr       */
+/*   Updated: 2022/10/10 17:41:14 by minkyeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,36 @@
 #include "gl_device.h"
 #include "gl_engine.h"
 #include "main.h"
+
+void	parse_sphere(t_device *device, char **line_split)
+{
+	// (1) check if sp has 4 char members
+	if (get_strs_count(line_split) != 4)
+	{
+		// TODO: print message of error log.
+		engine_exit(device, EXIT_FAILURE);
+	}
+	t_sphere *new_sphere = ft_calloc(1, sizeof(*new_sphere));
+
+	// TODO:  check if atof is successful, or has 3 member
+	char **each = ft_split(line_split[1], ',');
+	if (get_strs_count(each) != 3)
+	{
+		// TODO: print message of error log.
+		engine_exit(device, EXIT_FAILURE);
+	}
+	new_sphere->center = gl_vec3_3f(atof(each[0]), atof(each[1]), atof(each[2]));
+	free_split_char(each);
+	new_sphere->radius = atof(line_split[2]);
+	each = ft_split(line_split[3], ',');
+	if (get_strs_count(each) != 3)
+	{
+		// TODO: print message of error log.
+		engine_exit(device, EXIT_FAILURE);
+	}
+	new_sphere->color = gl_vec3_3f(atof(each[0]), atof(each[1]), atof(each[2]));
+	device->objects.spheres->push_back(device->objects.spheres, new_sphere);
+}
 
 void	parse_each(t_device *device, char **line_split)
 {
@@ -39,32 +69,7 @@ void	parse_each(t_device *device, char **line_split)
 	else if (ft_strncmp(line_split[0], "sp", ft_strlen(line_split[0])) == 0)
 	// if sphere
 	{
-		// (1) check if sp has 4 char members
-		if (get_strs_count(line_split) != 4)
-		{
-			// TODO: print message of error log.
-			engine_exit(device, EXIT_FAILURE);
-		}
-		t_sphere *new_sphere = ft_calloc(1, sizeof(*new_sphere));
-
-		// TODO:  check if atof is successful, or has 3 member
-		char **each = ft_split(line_split[1], ',');
-		if (get_strs_count(each) != 3)
-		{
-			// TODO: print message of error log.
-			engine_exit(device, EXIT_FAILURE);
-		}
-		new_sphere->center = gl_vec3_3f(atof(each[0]), atof(each[1]), atof(each[2]));
-		free_split_char(each);
-		new_sphere->radius = atof(line_split[2]);
-		each = ft_split(line_split[3], ',');
-		if (get_strs_count(each) != 3)
-		{
-			// TODO: print message of error log.
-			engine_exit(device, EXIT_FAILURE);
-		}
-		new_sphere->color = gl_vec3_3f(atof(each[0]), atof(each[1]), atof(each[2]));
-		device->objects.spheres->push_back(device->objects.spheres, new_sphere);
+		parse_sphere(device, line_split);
 	}
 	else if (ft_strncmp(line_split[0], "pl", ft_strlen(line_split[0])) == 0)
 	// if plane
@@ -92,7 +97,7 @@ void	parse_rt_file_to_device(t_device *device, char *file)
 	printf("file : %s\n", file);
 	// (1) check if file is able to open.
 	// WARN:  (1-1) if file name if not with .rt, return error!
-	if (ft_strnstr(file, ".rt", ft_strlen(file)) == NULL)
+	if (ft_strnstr(file, ".rt", ft_strlen(file)) == NULL && file[ft_strlen(file) - 3] == '.')
 	{
 		ft_putstr_fd("95\n", 1);
 		// ... error message
