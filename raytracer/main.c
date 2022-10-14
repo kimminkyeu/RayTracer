b8e064b2113b1577b32f71e719a803a2ac5e9b9d
Kyeu <minkyeki@student.42seoul.kr>
2022-10-14 21:53:27 +0900

    multi_thread complete

--- raytracer/main.c
+++ raytracer/main.c
@@ -6,7 +6,7 @@
 /*   By: minkyeki <minkyeki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 13:54:43 by minkyeki          #+#    #+#             */
-/*   Updated: 2022/10/14 21:27:44 by minkyeki         ###   ########.fr       */
+/*   Updated: 2022/10/14 21:52:50 by minkyeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,42 +25,26 @@ int	main(int ac, char **av)
 		return (EXIT_FAILURE);
 	}
 
+	const int WIDTH = 700;
+	const int HEIGHT = 700;
 	/** (1) Init engine && create image */
-	device = engine_init(1200, 800, "42 Mini-RayTracing");
-
-
-	// THREAD TEST
-	// ft_bzero(&device->thread_info, sizeof(device->thread_info));
-	t_thread_info *info = &device->thread_info;
-	const int THREAD_NUM = 24;
-	info->thread_group = ft_calloc(1, sizeof(t_thread) * THREAD_NUM);
-	info->thread_num = THREAD_NUM;
-	info->finished_thread_num = 0;
-	if (pthread_mutex_init(&(info->finished_num_mutex), NULL) != 0)
-	{
-		print_error_and_exit(device, "error in pthread_mutex_init()\n");
-	};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	engine_new_image(device, gl_vec2_2f(1200, 800), gl_vec2_2f(0,0), update);
+	device = engine_init(WIDTH, HEIGHT, "42 Mini-RayTracing");
+	engine_new_image(device, gl_vec2_2f(WIDTH, HEIGHT), gl_vec2_2f(0,0), update);
 
 	/** (2) Load files. (Map data etc...) then store data to [t_device] structure */
 	parse_rt_file_to_device(device, av[1]);
 
 	// TODO:  Delete later. just for parse check.
 	// print_rt_data(device);
+
+	// THREAD TEST
+	t_thread_info *info = &device->thread_info;
+	const int THREAD_NUM = 24;
+	info->thread_group = ft_calloc(1, sizeof(t_thread) * THREAD_NUM);
+	info->thread_num = THREAD_NUM;
+	info->finished_thread_num = 0;
+	if (pthread_mutex_init(&(info->finished_num_mutex), NULL) != 0)
+		print_error_and_exit(device, "error in pthread_mutex_init()\n");
 
 	/** (3) start rendering */
 	engine_render(device);
