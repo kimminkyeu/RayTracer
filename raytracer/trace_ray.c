8072f7c3020d31903801fe9389d05aa938053ffb
Kyeu <minkyeki@student.42seoul.kr>
2022-10-30 00:06:13 +0900

    norm again

--- raytracer/trace_ray.c
+++ raytracer/trace_ray.c
@@ -2,154 +2,146 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   trace_ray.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */ /*   By: minkyeki <minkyeki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minkyeki <minkyeki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/19 00:00:35 by minkyeki          #+#    #+#             */
-/*   Updated: 2022/10/19 00:09:23 by minkyeki         ###   ########.fr       */
+/*   Created: 2022/10/29 22:57:12 by minkyeki          #+#    #+#             */
+/*   Updated: 2022/10/29 23:59:54 by minkyeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "trace_ray.h"
+#include "hit.h"
+#include "ray.h"
 
+/**
+ * dx : for super sampling. (Length of a single pixel)
+ */
 int do_ray_tracing_and_return_color(t_device *device, t_image *img, int x, int y)
 {
 	(void)device;
+	const t_vec3	pixel_pos_world = transform_screen_to_camera_world(\
+							device->camera, img, gl_vec2_2f(x, y));
+	const float		dx = 2.0f / img->img_size.height;
+	t_vec3			trace_result;
+	int				final_color;
 
-	// const t_vec3 pixel_pos_world = transform_screen_to_world(img, gl_vec2_2f(x, y));
-	const t_vec3 pixel_pos_world = transform_screen_to_camera_world(device->camera, img, gl_vec2_2f(x, y));
-	const float	dx = 2.0f / img->img_size.height; // for super sampling. // world_coordinate 의 세로길이 / 이미지 세로 =  1pixel당 세로 길이 = 1pixel당 가로 길이.
-	t_vec3 trace_result = super_sampling_anti_aliasing(img, pixel_pos_world, dx, device->renderer_settings.antialiasing_level); // 마지막 정수가 0이면 픽셀 하나당 한 번 샘플링
-	trace_result = gl_vec3_clamp(trace_result, gl_vec3_1f(0.0f), gl_vec3_1f(255.0f));
-	int final_color = gl_get_color_from_vec4(gl_vec4_4f(trace_result.b, trace_result.g, trace_result.r, 0.0f));
+	trace_result = super_sampling_anti_aliasing(img, pixel_pos_world, dx, \
+					device->renderer_settings.antialiasing_level);
+	trace_result = gl_vec3_clamp(trace_result, \
+								gl_vec3_1f(0.0f), gl_vec3_1f(255.0f));
+	final_color = gl_get_color_from_vec4(\
+		gl_vec4_4f(trace_result.b, trace_result.g, trace_result.r, 0.0f));
 	return (final_color);
 }
 
-bool	is_pixels_are_same(t_vec3 *colors)
+void	update_4_sub_pixels(t_image *img, t_vec3 pixel_pos_world, const float dx, t_vec3 *sample_color)
 {
-	// check if 4 colors are same
-	int i = 0;
+	const float	sub_dx = 0.5f * dx;
+	int i;
+	int j;
+
+	j = 0;
+	while (j < 2)
+	{
+		i = 0;
+		while (i < 2)
+		{
+			t_vec3 sub_pos = gl_vec3_3f(pixel_pos_world.x + ((float)i * sub_dx),\
+										pixel_pos_world.y + ((float)j * sub_dx),\
+										pixel_pos_world.z);
+			t_vec3 ray_dir = gl_vec3_normalize(gl_vec3_subtract_vector(sub_pos, img->device_ptr->camera->pos));
+			t_ray pixel_ray = create_ray(sub_pos, ray_dir);
+			sample_color[j * 2 + i] = trace_ray(img->device_ptr, &pixel_ray, 1);
+			i++;
+		}
+		j++;
+	}
+
+}
+
+/** check if 4 colors are same */
+bool	is_4_sub_pixels_are_same(t_image *img, t_vec3 pixel_pos_world, const float dx, t_vec3 *colors)
+{
+	int	i;
+
+	i = 0;
+	update_4_sub_pixels(img, pixel_pos_world, dx, colors);
 	while (i < 3)
 	{
-		if (!(colors[i].r == colors[i+1].r && colors[i].g == colors[i+1].g && colors[i].b == colors[i+1].b))
+		if (!(colors[i].r == colors[i+1].r \
+			&& colors[i].g == colors[i+1].g \
+			&& colors[i].b == colors[i+1].b))
 			return (false);
 		i++;
 	}
 	return (true);
 }
 
-t_vec3 super_sampling_anti_aliasing(t_image *img, t_vec3 pixel_pos_world, const float dx, const int recursive_level)
+/**
+ * NOTE:  Super Sampling (4 Pixels Each)
+ * --------------------
+ * | 1(0.25)*  2(0.75)|
+ * |        *         |
+ * |------- * ---------
+ * | 3(0.25)* 4(0.75) |
+ * |        *         |
+ * --------------------*/
+
+t_vec3	do_recur_super_sampling(t_image *img, t_vec3 pixel_pos_world, const float sub_dx, const int super_sampling_recursive_level)
 {
-	// HIGH_RES mode가 아니거나 recursive_level이 0이면 하던대로 단일 Ray_tracing 진행.
-	if (img->device_ptr->is_high_resolution_render_mode == false || recursive_level == 0)
-	{
-		t_vec3 ray_dir = gl_vec3_normalize(gl_vec3_subtract_vector(pixel_pos_world, img->device_ptr->camera->pos));
-		t_ray pixel_ray = create_ray(pixel_pos_world, ray_dir);
-		return (trace_ray(img->device_ptr, &pixel_ray, img->device_ptr->renderer_settings.reflection_level));
-	}
+	t_vec3	pixel_color;
+	int 	i;
+	int 	j;
 
-	const float sub_dx = 0.5f * dx;
-	t_vec3 pixel_color = gl_vec3_1f(0.0f); // 재귀 첫 색상.?
-	pixel_pos_world.x = pixel_pos_world.x - sub_dx * 0.5f; // 0.25 와 0.75 지점을 구해야 하기 때문.
-	pixel_pos_world.y = pixel_pos_world.y - sub_dx * 0.5f; // 0.25 와 0.75 지점을 구해야 하기 때문.
-	int i = 0;
-	int j = 0;
-
-	// (1) 먼저 4개의 픽셀을 비교한다.
-	t_vec3 sample_color[4];
-	while (j < 2)
-	{
-		// j = 0일때 pos는 0.25 지점, j = 1일때 pos는 0.75 지점.
-		i = 0;
-		while (i < 2)
-		{
-			// i = 0일때 pos는 0.25 지점, i = 1일때 pos는 0.75 지점.
-			t_vec3 sub_pos = gl_vec3_3f(pixel_pos_world.x + ((float)i * sub_dx),\
-										pixel_pos_world.y + ((float)j * sub_dx),\
-										pixel_pos_world.z);
-
-			// 재귀 호출. (Recursive Super-Sampling)
-			t_vec3 ray_dir = gl_vec3_normalize(gl_vec3_subtract_vector(sub_pos, img->device_ptr->camera->pos));
-			t_ray pixel_ray = create_ray(sub_pos, ray_dir);
-			sample_color[j * 2 + i] = trace_ray(img->device_ptr, &pixel_ray, img->device_ptr->renderer_settings.reflection_level);
-			i++;
-		}
-		j++;
-	}
-	// (2) 만약 4개의 픽셀이 같다면, return. (supersampling 진행 하지 않기.)
-	if (is_pixels_are_same(sample_color))
-	{
-		// 어차피 다 같은 색이니 한가지 color 사용.
-		return (sample_color[0]);
-	}
-
-	// (3) 만약 4개의 픽셀이 하나라도 다르다면, super_sampling 진행.
-	i = 0;
+	pixel_color = gl_vec3_1f(0.0f);
 	j = 0;
 	while (j < 2)
 	{
-		// j = 0일때 pos는 0.25 지점, j = 1일때 pos는 0.75 지점.
 		i = 0;
 		while (i < 2)
 		{
-			// i = 0일때 pos는 0.25 지점, i = 1일때 pos는 0.75 지점.
 			t_vec3 sub_pos = gl_vec3_3f(pixel_pos_world.x + ((float)i * sub_dx),\
 										pixel_pos_world.y + ((float)j * sub_dx),\
 										pixel_pos_world.z);
-
-			// 재귀 호출. (Recursive Super-Sampling)
-			pixel_color = gl_vec3_add_vector(pixel_color, super_sampling_anti_aliasing(img, sub_pos, sub_dx, recursive_level - 1));
+			pixel_color = gl_vec3_add_vector(pixel_color, super_sampling_anti_aliasing(img, sub_pos, sub_dx, super_sampling_recursive_level - 1));
 			i++;
 		}
 		j++;
 	}
-	return (gl_vec3_multiply_scalar(pixel_color, 0.25f)); // pixel의 색상을 평균내는 부분 (나누기 4)
+	return (gl_vec3_multiply_scalar(pixel_color, 0.25f));
 }
 
-// 광선을 추적해서 부딪힌 지점의 color를 반환.
-t_vec3 trace_ray(t_device *device, const t_ray *ray, const int recursive_level)
+t_vec3 super_sampling_anti_aliasing(t_image *img, t_vec3 pixel_pos_world, const float dx, const int super_sampling_recursive_level)
 {
-	if (recursive_level < 0)
-		return (gl_vec3_1f(0.0f));
+	const float	sub_dx = 0.5f * dx;
+	t_vec3		sample_colors[4];
+	t_vec3		ray_dir;
+	t_ray		pixel_ray;
 
-	// (0) Render first hit
-	t_hit hit = find_closet_collision(device, ray);
-
-	// * If ray hit object, then calculate with Phong-Shading-model.
-	if (hit.distance >= 0.0f) // if has hit.
+	if (img->device_ptr->is_high_resolution_render_mode == false || super_sampling_recursive_level == 0)
 	{
-		return (phong_shading_model(device, ray, hit, recursive_level));
+		ray_dir = gl_vec3_normalize(gl_vec3_subtract_vector(pixel_pos_world, img->device_ptr->camera->pos));
+		pixel_ray = create_ray(pixel_pos_world, ray_dir);
+		return (trace_ray(img->device_ptr, &pixel_ray, img->device_ptr->renderer_settings.reflection_level));
 	}
-	return (gl_vec3_1f(0.0f)); // return black
+	pixel_pos_world.x = pixel_pos_world.x - sub_dx * 0.5f;
+	pixel_pos_world.y = pixel_pos_world.y - sub_dx * 0.5f;
+	if (is_4_sub_pixels_are_same(img, pixel_pos_world, dx, sample_colors))
+		return (sample_colors[0]);
+	else
+		return (do_recur_super_sampling(img, pixel_pos_world, sub_dx, super_sampling_recursive_level));
 }
 
-/** ---------------------------------------------------------------------------------------------- */
-
-
-/* ------------------------------------------------------------------------------------------- *
- * |  NOTE:  Main Ray-tracing Algorithm !   => 그림자가 자연스럽게 Blending 되려면?
-
- 개선 가능 사항:
- (1) ambient light을 그대로 물체의 ambient_color로 사용하고 있기에, 그림자의 색이 모두 동일하게 나온다.
- --> 그림자를 처리하는 다른 방법이 있는 걸까?
-
- ---------------------------------------------------------------------------------------------- */
-//  # include <unistd.h>
-//  # include <stdio.h>
-
-t_vec3 calculate_diffusse_specular_shadow_from_light(t_device *device, const t_ray *ray, t_hit hit, t_light* light)
+t_vec3 calculate_phong(t_device *device, const t_ray *ray, t_hit hit, t_light* light)
 {
 	t_vec3 final_color = gl_vec3_1f(0.0f);
+	t_vec3 phong_color = gl_vec3_1f(0.0f);
 	// 그림자 처리. 아주 작은 값만큼 광원을 향해 이동시켜야 hit_point로 부터 충돌처리를 피할 수 있다.
 	const t_vec3 hit_point_to_light = gl_vec3_normalize(gl_vec3_subtract_vector(light->pos, hit.point));
 
-
-
-	// if (gl_vec3_dot(hit_point_to_light, hit.normal) < 0.0f) return (gl_vec3_3f(255, 0, 0));
-
-
 	// 만약 [hit_point+살짝 이동한 지점] 에서  shadow_ray를 광원을 향해 쐈는데, 충돌이 감지되면 거긴 그림자로 처리.
 	// WARN:  아래 그림자에서 사용된 1e-4f는 반사/반투명 물체에서 문제가 발생 할 수 있음.
-	t_vec3 phong_color = gl_vec3_1f(0.0f);
 	phong_color = gl_vec3_multiply_scalar(device->ambient_light->color, device->ambient_light->brightness_ratio);
 
 
@@ -220,72 +212,29 @@ t_vec3 calculate_diffusse_specular_shadow_from_light(t_device *device, const t_r
 	return (final_color);
 }
 
-t_vec3 phong_shading_model(t_device *device, const t_ray *ray, t_hit hit, const int recursive_level)
+// d - 2( n . d )n
+// move ray slightly to it's direction to avoid early hit.
+t_vec3 calculate_reflection(t_device *device, const t_ray *ray, t_hit hit, const int reflection_recursive_level)
 {
-	t_vec3 final_color = gl_vec3_1f(0.0f);
+	const t_vec3	reflected_ray_dir = gl_vec3_subtract_vector(ray->direction, (gl_vec3_multiply_scalar(gl_vec3_multiply_scalar(hit.normal, gl_vec3_dot(ray->direction, hit.normal)), 2.0f)));
+	const t_vec3	hit_point_offset = gl_vec3_add_vector(hit.point, gl_vec3_multiply_scalar(reflected_ray_dir, 1e-4f));
+	const t_ray		reflected_ray = create_ray(hit_point_offset, reflected_ray_dir);
+	const t_vec3	reflected_color = trace_ray(device, &reflected_ray, reflection_recursive_level - 1);
+	return (gl_vec3_multiply_scalar(reflected_color, hit.obj->material.reflection));
+}
 
-	// * ---------------------------------------------------------------------------------
-	// * 1017. Normal Map 구현 (내가 생각하는 normal_map 구현기. 정말 이게 맞는지는 해보고 체크. ----------------
-	if (hit.obj->normal_texture != NULL && device->is_high_resolution_render_mode == true)
-	{
-	// 	static int lock1 = 0;
-	// 	if (lock1 == 0)
-	// 	{
-	// 		if (hit.distance > 0.0f && hit.point.x < -0.1f && hit.point.y > 0.1f)
-	// 		{
-	// 			printf("=== x%f y%f tangent:%f/%f/%f\n", hit.point.x, hit.point.y, hit.tangent.x, hit.tangent.y, hit.tangent.z);
-	// 			lock1 = 1;
-	// 		}
-	// 	}
-	// 	if (lock1 == 1)
-	// 	{
-	// 		if (hit.distance > 0.0f && hit.point.x > 0.1f && hit.point.y < -0.1f)
-	// 		{
-	// 			printf("=== x%f y%f tangent:%f/%f/%f\n", hit.point.x, hit.point.y, hit.tangent.x, hit.tangent.y, hit.tangent.z);
-	// 			lock1 = 3;
-	// 		}
-	// 	}
-		// 참고 자료
-		// https://www.youtube.com/watch?v=6_-NNKc4lrk
-		// Normal Map을 이용한다면, _diff 의 값이 달리질 것이다.
-		// hit.normal이 normal_map에 의해 추가적으로 계산되어야 한다.
-		hit.normal = sample_normal_map(&hit);
-	}
-	// * -------------------------------------------------------------------------------------------
-	// caculate from each light.
-	t_vec3 phong_color = gl_vec3_1f(0.0f);
-	size_t i = 0;
-	while (i < device->point_lights->size)
-	{
-		t_light	*light_each = device->point_lights->data[i];
-		phong_color = gl_vec3_add_vector(phong_color, calculate_diffusse_specular_shadow_from_light(device, ray, hit, light_each));
-		i++;
-	}
-	final_color = phong_color;
-	return (phong_color);
-
-	if (hit.obj->material.reflection)
-	{
-		// 여기에 반사 구현
-		// 수치 오류 주의
-		// 반사광이 반환해준 색을 더할 때의 비율은 hit.obj->reflection
-		// d - 2( n . d )n
-		const t_vec3 reflected_ray_dir = gl_vec3_subtract_vector(ray->direction, (gl_vec3_multiply_scalar(gl_vec3_multiply_scalar(hit.normal, gl_vec3_dot(ray->direction, hit.normal)), 2.0f)));
-		// 살짝 위로 떨궈야 hit 계산시에 충돌처리 되지 않음.
-		const t_vec3 hit_point_offset = gl_vec3_add_vector(hit.point, gl_vec3_multiply_scalar(reflected_ray_dir, 1e-4f));
-		const t_ray reflected_ray = create_ray(hit_point_offset, reflected_ray_dir);
-		const t_vec3 reflected_color = trace_ray(device, &reflected_ray, recursive_level - 1);
-		final_color = gl_vec3_add_vector(final_color, gl_vec3_multiply_scalar(reflected_color, hit.obj->material.reflection));
-	}
-
-	if (hit.obj->material.transparency)
-	{
-		// * 참고
-		// * (1) https://samdriver.xyz/article/refraction-sphere (그림들이 좋아요)
-		// * (2) https://www.scratchapixel.com/lessons/3d-basic-rendering/introduction-to-shading/reflection-refraction-fresnel (오류있음)
-		// * (3) https://web.cse.ohio-state.edu/~shen.94/681/Site/Slides_files/reflection_refraction.pdf (슬라이드가 보기 좋지는 않지만 정확해요)
-		// * (4) https://lifeisforu.tistory.com/384
-
+// * 참고
+// * (1) https://samdriver.xyz/article/refraction-sphere (그림들이 좋아요)
+// * (2)
+// https://www.scratchapixel.com/lessons/3d-basic-rendering/introduction-to-shading/reflection-refraction-fresnel
+// (오류있음)
+// * (3)
+// https://web.cse.ohio-state.edu/~shen.94/681/Site/Slides_files/reflection_refraction.pdf
+// (슬라이드가 보기 좋지는 않지만 정확해요)
+// * (4) https://lifeisforu.tistory.com/384
+// Fresnel 효과는 생략되었습니다.
+t_vec3 calculate_refraction(t_device *device, const t_ray *ray, t_hit hit, const int reflection_recursive_level)
+{
 		const float ior = 1.5f; // Index of refraction (유리: 1.5, 물: 1.3)
 
 		float eta; // sinTheta1 / sinTheta2
@@ -319,32 +268,70 @@ t_vec3 phong_shading_model(t_device *device, const t_ray *ray, t_hit hit, const 
 
 		t_vec3 offset = gl_vec3_multiply_scalar(refracted_direction, 0.0001f);
 		t_ray refraction_ray = create_ray(gl_vec3_add_vector(hit.point, offset), refracted_direction);
-
-		const t_vec3 refracted_color = trace_ray(device, &refraction_ray, recursive_level - 1);
-		final_color = gl_vec3_add_vector(final_color, gl_vec3_multiply_scalar(refracted_color, hit.obj->material.transparency));
-		// Fresnel 효과는 생략되었습니다.
-	}
-	return (final_color);
+		const t_vec3 refracted_color = trace_ray(device, &refraction_ray, reflection_recursive_level - 1);
+		return (gl_vec3_multiply_scalar(refracted_color, hit.obj->material.transparency));
 }
 
+t_vec3 calculate_pixel_color(t_device *device, const t_ray *ray, t_hit hit, const int reflection_recursive_level)
+{
+	t_vec3	phong_color;
+	t_light	*light_each;
+	size_t	i;
 
-// device에 있는 모든 obj를 돌면서, 가장 가까운 충돌 지점을 계산.
+	if (hit.obj->normal_texture != NULL && device->is_high_resolution_render_mode == true)
+		hit.normal = sample_normal_map(&hit);
+	i = 0;
+	phong_color = gl_vec3_1f(0.0f);
+	while (i < device->point_lights->size)
+	{
+		light_each = device->point_lights->data[i];
+		phong_color = gl_vec3_add_vector(phong_color, calculate_phong(device, ray, hit, light_each));
+		i++;
+	}
+	if (hit.obj->material.reflection)
+		phong_color = gl_vec3_add_vector(phong_color, calculate_reflection(device, ray, hit, reflection_recursive_level));
+	if (hit.obj->material.transparency)
+		phong_color = gl_vec3_add_vector(phong_color, calculate_refraction(device, ray, hit, reflection_recursive_level));
+	return (phong_color);
+}
+
+/* ---------------------------------------
+ * |  NOTE:  Main Ray-tracing Algorithm  |
+ ----------------------------------------*/
+
+/** trace each ray's hit-object, then calcuate color with Phong-model. */
+t_vec3 trace_ray(t_device *device, const t_ray *ray, const int reflection_recursive_level)
+{
+	t_hit	hit;
+
+	if (reflection_recursive_level < 0)
+		return (gl_vec3_1f(0.0f));
+	hit = find_closet_collision(device, ray);
+	if (hit.distance >= 0.0f)
+	{
+		return (calculate_pixel_color(device, ray, hit, reflection_recursive_level));
+	}
+	else
+		return (gl_vec3_1f(0.0f));
+}
+
 t_hit find_closet_collision(t_device *device, const t_ray *ray)
 {
-	float closest_distance = FLT_MAX;
-	t_hit closest_hit = create_hit(-1.0f, gl_vec3_1f(0.0f), gl_vec3_1f(0.0f));
+	float	closest_distance;
+	t_hit	closest_hit;
+	t_hit	hit_result;
+	size_t	i;
 
-	size_t i = 0;
+	closest_distance = FLT_MAX;
+	closest_hit = create_hit(-1.0f, gl_vec3_1f(0.0f), gl_vec3_1f(0.0f));
+	i = 0;
 	while (i < device->objects->size)
 	{
-		// ... find hit
-		t_hit hit = check_ray_collision(ray, device->objects->data[i]);
-
-		// 모든 물체에 대해서 충돌을 계산하고, 그 중에서 d값이 더 작은 경우 그 값을 hit으로 만들면 된다.
-		if (hit.distance >= 0.0f && hit.distance < closest_distance)
+		hit_result = check_ray_collision(ray, device->objects->data[i]);
+		if (hit_result.distance >= 0.0f && hit_result.distance < closest_distance)
 		{
-			closest_distance = hit.distance;
-			closest_hit = hit;
+			closest_distance = hit_result.distance;
+			closest_hit = hit_result;
 			closest_hit.obj = device->objects->data[i];
 		}
 		i++;
